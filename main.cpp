c7e5f6c3e97194e872cfea740521d3dd9ded844c
Angello <angello.zuloaga@utec.edu.pe>
2022-05-24 15:49:39 -0500

    factorial

--- main.cpp
+++ main.cpp
@@ -2,8 +2,15 @@
 #include <vector>
 using namespace std;
 
+int factorial(int a) {
+    if (a==1) {
+        return 1;
+    }
+    return a * factorial(a-1);
+}
+
+
 int main() {
-    std::cout << "Hola" << endl;
-    std::cout << "asd";
+    cout << factorial(5) << endl;
     return 0;
 }
